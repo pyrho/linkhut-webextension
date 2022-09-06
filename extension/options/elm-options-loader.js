795746a12de423109c4b5d4dd916e292fd13d75b
pyrho <pyrho@25.wf>
2022-09-06 20:52:01 +0200

    fix(popup): filter bg msg to popup on channel

--- extension/options/elm-options-loader.js
+++ extension/options/elm-options-loader.js
@@ -1,41 +1,42 @@
 (async function elmbootstrap() {
-    const settings =
-        (await browser.storage.local.get("settings"))?.settings ?? {};
+  const settings =
+    (await browser.storage.local.get("settings"))?.settings ?? {};
 
-    var app = Elm.Options.init({
-        node: document.getElementById("elm"),
-        flags: {
-            clientId: settings?.clientId ?? null,
-            clientSecret: settings?.clientSecret ?? null,
-            webUrl: settings?.webUrl ?? null,
-            apiUrl: settings?.apiUrl ?? null,
+  var app = Elm.Options.init({
+    node: document.getElementById("elm"),
+    flags: {
+      clientId: settings?.clientId ?? null,
+      clientSecret: settings?.clientSecret ?? null,
+      webUrl: settings?.webUrl ?? null,
+      apiUrl: settings?.apiUrl ?? null,
+    },
+  });
+
+  app.ports.sendMessage.subscribe(async (payload) => {
+    if (payload.action === "save") {
+      await browser.storage.local.set({
+        settings: {
+          webUrl: payload.data.webUrl,
+          apiUrl: payload.data.apiUrl,
+          clientId: payload.data.clientId,
+          clientSecret: payload.data.clientSecret,
         },
-    });
+      });
 
-    app.ports.sendMessage.subscribe(async payload => {
-        // The popup has succesfully saved the link
-        // Now close the popup
-        if (payload.action === "save") {
-            await browser.storage.local.set({
-                settings: {
-                    webUrl: payload.data.webUrl,
-                    apiUrl: payload.data.apiUrl,
-                    clientId: payload.data.clientId,
-                    clientSecret: payload.data.clientSecret,
-                },
-            });
+      app.ports.messageReceiver.send("success");
+    } else if (payload.action === "reset") {
+      await browser.storage.local.remove("settings");
+      app.ports.messageReceiver.send("resetDone");
+    }
+  });
 
-            app.ports.messageReceiver.send("success");
-        } else if (payload.action === "reset") {
-            await browser.storage.local.remove("settings");
-            app.ports.messageReceiver.send("resetDone");
-        }
-    });
+  browser.runtime.onMessage.addListener((msg) => {
+    // This is needed because we expect a `Nothing` in Elm land.
+    if (!msg.data) msg.data = null;
 
-    browser.runtime.onMessage.addListener(msg => {
-        // This is needed because we expect a `Nothing` in Elm land.
-        if (!msg.data) msg.data = null;
+    // We only care about this message for the options page
+    if (msg.action !== "resetDone") return;
 
-        app.ports.messageReceiver.send(msg);
-    });
+    app.ports.messageReceiver.send(msg);
+  });
 })();
